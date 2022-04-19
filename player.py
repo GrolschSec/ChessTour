8599d4a8d93e771f6bfe4156ec1ce64eac0d0166
Romain Louvrier <romain-louvrier@outlook.fr>
2022-04-19 15:56:04 +0200

    Player issue with pycharm

---
+++ player.py
@@ -0,0 +1,8 @@
+class Player:
+
+    def __int__(self, name=str, lastname=str, birthday=str, sexe=str, classment=int):
+        self.name = name
+        self.lastname = lastname
+        self.birthday = birthday
+        self.sexe = sexe
+        self.classment = classment
