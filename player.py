8e80143c36b2640614eaf7aa654c1f700262204c
Romain Louvrier <romain-louvrier@outlook.fr>
2022-04-20 14:42:30 +0200

    Adding files views, models, controllers

--- player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, name, lastname, birthday, sex, classment):
-        self.name = name
-        self.lastname = lastname
-        self.birthday = birthday
-        self.sex = sex
-        self.classment = classment
--- tournament.py
+++
@@ -1,6 +0,0 @@
-class Tournament:
-    def __int__(self, players_num):
-        self.players = players_num
-
-    def get_match_numbers(self):
-        pass
